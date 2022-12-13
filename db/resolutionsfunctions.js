8f8b7977e36351dfb82f564a41b20b7e44428923
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-12-14 00:21:16 +0100

    Minor change.

--- db/resolutionsfunctions.js
+++ db/resolutionsfunctions.js
@@ -2,6 +2,6 @@
 // with a name, so that i can have "domain names" for the functions
 
 module.exports = {
-  // bishops, actually we could make this more simple...
+  // resolutions, actually we could make this more simple...
   edm: require("./functions/bishops/edm"),
 };
