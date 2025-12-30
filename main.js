e00ce14c0dd0f5a733edc1eed88071f6c94ced61
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-12-30 16:28:02 +0100

    Removing express from main and adding fastify.

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-const app = require("./server");
+const fastify = require("fastify")({ logger: { level: "info" } });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}/`);
+const helmet = require("@fastify/helmet");
+
+require("dotenv").config();
+
+// no sé por qué las jodidas peticiiones piden un favicon. Con esto
+// evitamos en el log q ponga que no existe.
+fastify.addHook("onRequest", (request, reply, done) => {
+  if (request.url === "/favicon.ico") {
+    reply.code(204).send();
+  } else {
+    done();
+  }
+});
+
+// el asunto es este: https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+// cuando hace un POST realmente pregunta antes al servidor con el método OPTIONS...
+fastify.register(require("@fastify/cors"), {
+  origin: ["http://localhost:3000"],
+  methods: "GET,POST,OPTIONS",
+});
+
+fastify.register(require("@fastify/rate-limit"), {
+  max: 180,
+  timeWindow: "1 minute",
+});
+
+// We declare a route
+fastify.register(require("./src/routes/general"), { prefix: "/general" });
+// fastify.register(require("./src/routes/static"), { prefix: "/static" });
+
+fastify.listen({ port: process.env.PORT }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 });
