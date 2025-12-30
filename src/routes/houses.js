e5882586239143537e081f646a84ef460113840b
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-12-30 20:25:49 +0100

    Adapting routes.

---
+++ src/routes/houses.js
@@ -0,0 +1,41 @@
+const { db, pgp } = require("../db/dbconnect");
+
+const {
+  sqlHousesGeneralData,
+  sqlHousesProvinces,
+  sqlHousesSocio,
+} = require("../helpers/readsqls/readsqls");
+
+async function routes(fastify, options) {
+  fastify.get("/statistics/general/", async (request, reply) => {
+    try {
+      const housesList = await db.many(sqlHousesGeneralData);
+      return reply.status(200).send(housesList);
+    } catch (err) {
+      console.log(err);
+      return reply.status(400).send(err);
+    }
+  });
+
+  fastify.get("/houses/", async (request, reply) => {
+    try {
+      const housesList = await db.many(sqlHousesSocio);
+      return reply.status(200).send(housesList);
+    } catch (err) {
+      console.log(err);
+      return reply.status(400).send(err);
+    }
+  });
+
+  fastify.get("/provinces/", async (request, reply) => {
+    try {
+      const provList = await db.many(sqlHousesProvinces);
+      return reply.status(200).send(provList);
+    } catch (err) {
+      console.log(err);
+      return reply.status(400).send(err);
+    }
+  });
+}
+
+module.exports = routes;
