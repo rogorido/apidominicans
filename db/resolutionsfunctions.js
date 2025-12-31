36043ddef90697ed437fb006738deba288f04ee4
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-12-31 16:32:16 +0100

    Deleting old files.

--- db/resolutionsfunctions.js
+++
@@ -1,7 +0,0 @@
-// we import all files with the functions and export an object
-// with a name, so that i can have "domain names" for the functions
-
-module.exports = {
-  // resolutions, actually we could make this more simple...
-  chaps: require("./functions/resolutions/dbfunctions1570"),
-};
